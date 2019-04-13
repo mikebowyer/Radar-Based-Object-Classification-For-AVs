dc434b258ee0c7695268ea18a78d4ad30a8c11b5
Mike Bowyer <mbowyer@umich.edum>
2019-04-12 20:22:39 -0400

    Added some plotting of categories and updated git ignore to ignore data folder

--- readInDataFromCSV.py
+++ readInDataFromCSV.py
@@ -10,5 +10,26 @@ import matplotlib.pyplot as plt
 
 path = "Data/labelledRadarPoints.csv"
 
-pointData = pd.read_csv(path, index_col=False)
+#Load in radar points data frame
+radarPointDF = pd.read_csv(path, index_col=False)
 
+print(radarPointDF.head(3))
+radarPointDF.iloc[0]
+
+#Plot Occurances of different categories
+fig, ax = plt.subplots()
+ax.set_title('Frequency of Label Categories in Data Set')
+ax.set_xlabel('Category Types')
+ax.set_ylabel('Occurances of each Category')
+radarPointDF['category'].value_counts().plot(ax=ax,kind='bar')
+plt.show()
+
+
+humanPeds=radarPointDF['category']=='human.pedestrian.construction_worker'
+humanPedsDF=radarPointDF[humanPeds]
+
+
+x=radarPointDF.rcs
+y=radarPointDF.category
+
+plt.scatter(x, y)
