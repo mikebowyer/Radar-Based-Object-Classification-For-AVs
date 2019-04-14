31ec08f8df73f411d8a86ffad76f8d48d0bf8c66
Mike Bowyer <mbowyer@umich.edum>
2019-04-14 18:29:54 -0400

    Updated the save paths and data prep for logistic regression

--- LogisticRegression/LogRegression_w_L1_Regularization.py
+++ LogisticRegression/LogRegression_w_L1_Regularization.py
@@ -12,7 +12,7 @@ from sklearn import model_selection
 import pickle
 
 #Load in cleansed radar points data frame
-path = "Data/CleansedRadarPoints.csv"
+path = "../Data/CleansedRadarPoints.csv"
 pointDF = pd.read_csv(path, index_col=False)
 
 # =============================================================================
@@ -21,17 +21,11 @@ pointDF = pd.read_csv(path, index_col=False)
 #
 # =============================================================================
 X= pointDF[['x','y','dyn_prop','rcs','vx_comp','vy_comp','ambig_state','x_rms','y_rms','invalid_state','pdh0','vx_rms','vy_rms']].to_numpy()
-
-pointDF["BasicCategory"] = pointDF["BasicCategory"].astype('category')
-pointDF.dtypes
-pointDF["BasicCategoryNum"] = pointDF["BasicCategory"].cat.codes
-pointDF.head()
-
 y=pointDF[['BasicCategoryNum']]
 
 #Actualy Logistic Regression Model Run
 #logReg = LogisticRegressionCV(cv=10,penalty='l1', multi_class='multinomial',solver='saga',n_jobs=4).fit(X,y)
-filename = 'LogRegCV_10Folds.sav'
+#filename = 'LogRegCV_10Folds.sav'
 #pickle.dump(logReg, open(filename, 'wb')) #export model so you don't need to rerun it and wait forever
 
 # =============================================================================
@@ -40,6 +34,7 @@ filename = 'LogRegCV_10Folds.sav'
 # 
 # =============================================================================
 #read in the model since it was already ran
+filename='savedModels/LogRegCV_10Folds.sav'
 logReg = pickle.load(open(filename, 'rb'))
 print("training score : %.3f " % (logReg.score(X, y)))
 coef = logReg.coef_
