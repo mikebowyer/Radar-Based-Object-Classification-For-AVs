a3fa6baaa97a32f89e22e0ace21523e3611e562b
Mike Bowyer <mbowyer@umich.edum>
2019-04-14 17:57:55 -0400

    changed path to modified data

--- dataCleansing.py
+++ dataCleansing.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-path = "Data/labelledRadarPoints.csv"
+path = "../Data/labelledRadarPoints.csv"
 
 #Load in radar points data frame
 radarPointDF = pd.read_csv(path, index_col=False)
