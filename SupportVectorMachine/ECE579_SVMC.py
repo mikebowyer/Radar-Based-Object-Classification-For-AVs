c69848f730192dad95d0d51ad4bdd503c03d52e1
Jeopardy203 <brandon.fedoruk@gmail.com>
2019-04-21 12:46:31 -0400

    Prediction and Evaluation
    Modified prediciton and evaluation for confusion matrices and classification reports.

--- SupportVectorMachine/ECE579_SVMC.py
+++ SupportVectorMachine/ECE579_SVMC.py
@@ -3,36 +3,14 @@
 @author Brandon Fedoruk
 """
 import pandas as pd
-from sklearn.svm import SVC
+from sklearn import svm
 import time
 import pickle
 from sklearn.metrics import classification_report, confusion_matrix  
-#filePath = "CleansedRadarPoints.csv"
-#data = pd.read_csv(filePath, index_col=False)
 
-#X = data[['x','y','dyn_prop','rcs','vx_comp','vy_comp','ambig_state','x_rms','y_rms','invalid_state','pdh0','vx_rms','vy_rms']]
-#y = data[['BasicCategoryNum']]
-
-#from sklearn.model_selection import KFold
-#k-fold CV (5 folds)
-#kf = KFold(n_splits=5)
-#KFold(n_splits=5, random_state=None, shuffle=False)
-#Generate the k-fold sets
-#for train_index, test_index in kf.split(X):
-  #  X_train, X_test = X[train_index], X[test_index]
-  #  y_train, y_test = y[train_index], y[test_index]
-
-
-#from sklearn.model_selection import train_test_split  
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
-
-#train,test = train_test_split(data, test_size = 0.20)  
-#train.to_csv('data_train.csv')
-#test.to_csv('data_test.csv')
-
-
-filePath1   = "data_train.csv"
-filePath2   = "data_test.csv"
+# Initial read of sanitized data MUST be in working directory
+filePath1   = "UnderSampled_TrainSet.csv"
+filePath2   = "UnderSampled_TestSet.csv"
 train_data = pd.read_csv(filePath1, index_col=False)
 test_data  = pd.read_csv(filePath2, index_col=False)
 #clf = svm.SVC(C=1.0, cache_size=2000, class_weight='balanced', coef0=0.0,
@@ -40,31 +18,51 @@ test_data  = pd.read_csv(filePath2, index_col=False)
 #              max_iter=-1, probability=False, random_state=None, shrinking=True,
 #              tol=0.1, verbose=False)
 
-
-X_train = train_data[['x','y','dyn_prop','rcs','vx_comp','vy_comp','ambig_state','x_rms','y_rms','invalid_state','pdh0','vx_rms','vy_rms']]
+# Pre process dataset.
+X_train = train_data[['dyn_prop','rcs','vx_comp','vy_comp','ambig_state','x_rms','y_rms','invalid_state','pdh0','vx_rms','vy_rms']]
 y_train = train_data['BasicCategoryNum']
 
-X_train = X_train.head(10)
-y_train = y_train.head(10)
-
-X_test  = test_data[['x','y','dyn_prop','rcs','vx_comp','vy_comp','ambig_state','x_rms','y_rms','invalid_state','pdh0','vx_rms','vy_rms']]
+X_test  = test_data[['dyn_prop','rcs','vx_comp','vy_comp','ambig_state','x_rms','y_rms','invalid_state','pdh0','vx_rms','vy_rms']]
 y_test  = test_data['BasicCategoryNum']
 
-clf = SVC(gamma='scale', decision_function_shape='ovo',cache_size=5000,tol=0.1)
-#train
+#generate model coefficents
+clf = svm.SVC(gamma='scale', decision_function_shape='ovo',cache_size=5000,tol=0.1)
+#train model
 start = time.clock()
-svm_output = clf.fit(X_train,y_train) 
+clf.fit(X_train,y_train) 
 print (time.clock() - start)
 
 
 #export model so you don't need to rerun it and wait forever
-filename = 'McSVM.sav' 
-pickle.dump(svm_output, open(filename, 'wb')) 
+filename= 'McSVM_noxy.sav' 
+pickle.dump(clf, open(filename, 'wb')) 
 
+# Import model for post information processing
+svm_import = pickle.load(open(filename, 'rb'))
 
-#Predict
-pred = clf.predict(X_test,y_test)  
+#Prediction on the test set
+pred_test = svm_import.predict(X_test)
+#Prediction on the training set
+pred_train = svm_import.predict(X_train)  
 
-#Evaluate
-print(confusion_matrix(y_test,pred))  
-print(classification_report(y_test,pred))  
+#Evaluate on the training dataset to determine severity of overfitting
+print(confusion_matrix(y_train,
+                       pred_train,
+                       title='SVM With RBF Confusion Matrix On Training Set',
+                       target_names=['Bicycle','Bus','Construction','Motorcycle','Pass. Veh.','Pedestrian','Tractor','Trailer']))
+#print(confusion_matrix_trainingset)
+print(classification_report(y_train,
+                            pred_train,
+                            title='SVM With RBF Classification Report On Training Set',
+                            target_names=['Bicycle','Bus','Construction','Motorcycle','Pass. Veh.','Pedestrian','Tractor','Trailer']))
+
+#Evaluate on the test dataset
+print(confusion_matrix(y_test,
+                       pred_test,
+                       title='SVM With RBF Confusion Matrix On Test Set',
+                       target_names=['Bicycle','Bus','Construction','Motorcycle','Pass. Veh.','Pedestrian','Tractor','Trailer']))
+
+print(classification_report(y_test,
+                            pred_test,
+                            title='SVM With RBF Classification Report On Test Set',
+                            target_names=['Bicycle','Bus','Construction','Motorcycle','Pass. Veh.','Pedestrian','Tractor','Trailer']))
