aaa8bf4016137fb59cc83a312d266b07f303c149
Mike Bowyer <mbowyer@umich.edum>
2019-04-20 21:52:04 -0400

    Created undersampled data set.

--- CleaningAndIntrospection/train&testSetCreation.py
+++ CleaningAndIntrospection/train&testSetCreation.py
@@ -7,18 +7,21 @@ Created on Wed Apr 17 21:41:39 2019
 import pandas as pd 
 import numpy as np
 from imblearn.combine import SMOTETomek
+from imblearn.under_sampling import TomekLinks
 from imblearn.over_sampling import SMOTENC
 from sklearn.model_selection import train_test_split  
+from imblearn.under_sampling import ClusterCentroids
+from imblearn.under_sampling import RandomUnderSampler
 import time
 #read in clean data set
 path = "../Data/CleansedRadarPoints.csv"
 pointDF = pd.read_csv(path, index_col=False)
 columns=['x','y','dyn_prop','rcs','vx_comp','vy_comp','ambig_state','x_rms','y_rms','invalid_state','pdh0','vx_rms','vy_rms','BasicCategoryNum']
 #divide data from labels
-pointDF.x_rms.value_counts()
+print(pointDF.BasicCategory.value_counts())
 # =============================================================================
 # 
-# CREATE OVERSAMPLED DATASET
+# CREATE UNDERSAMPLED DATASET
 # 
 # =============================================================================
 
@@ -34,56 +37,77 @@ pointDF["vy_rms"] = pointDF["vy_rms"].astype('category')
 
 X=pointDF[['x','y','dyn_prop','rcs','vx_comp','vy_comp','ambig_state','x_rms','y_rms','invalid_state','pdh0','vx_rms','vy_rms']]
 y=pointDF['BasicCategoryNum']
-sm = SMOTENC(categorical_features=[2,6,7,8,9,10,11,12],n_jobs=6)
 
 start = time.clock()
-smt = SMOTETomek(smote=SMOTENC,ratio='auto')
-X_smt, y_smt = sm.fit_sample(X, y)
+
+#Remove passenger car samples randomly
+desiredSampleCounts= {
+		4 : 75000}
+rus = RandomUnderSampler(sampling_strategy=desiredSampleCounts)
+X_undersampled, y_undersampled = rus.fit_resample(X, y)
+
+#Remove tractor samples randomly
+desiredSampleCounts= {
+		6 : 75000}
+rus = RandomUnderSampler(sampling_strategy=desiredSampleCounts)
+X_undersampled, y_undersampled = rus.fit_resample(X_undersampled, y_undersampled)
+print(np.bincount(y_undersampled))
+
+#Remove Tomek Pairs
+underSampleObj = TomekLinks(sampling_strategy='all',n_jobs=5)
+X_undersampledTomek, y_undersampledTomek = underSampleObj.fit_resample(X_undersampled, y_undersampled)
+print(np.bincount(y_undersampledTomek))
+
+#Over sample minority classes to match majority classes
+overSampleObj = SMOTENC(categorical_features=[2,6,7,8,9,10,11,12],n_jobs=6)
+X_final, y_final = overSampleObj.fit_resample(X_undersampledTomek, y_undersampledTomek)
+print(np.bincount(y_final))
+
 print (time.clock() - start)
 # =============================================================================
 # 
 # Reconstruct full data frame to include BasicCategory
 # 
 # =============================================================================
-y_smt_reshape=y_smt.reshape((-1,1))
-fullOverSampled=np.append(X_smt,y_smt_reshape,1)
-fullOverSampledDf=pd.DataFrame(fullOverSampled,columns=columns)
+y_final_reshape=y_final.reshape((-1,1))
+underSampledData=np.append(X_final,y_final_reshape,1)
+underSampledDf=pd.DataFrame(underSampledData,columns=columns)
 
-fullOverSampledDf['BasicCategoryNum'].value_counts()
+underSampledDf['BasicCategoryNum'].value_counts()
 #Create Pedestrian Basic Category
-peds=fullOverSampledDf[fullOverSampledDf['BasicCategoryNum']==5]
+peds=underSampledDf[underSampledDf['BasicCategoryNum']==5]
 peds = peds.assign(BasicCategory='Pedestrian')
-#Create Passenger Vehicle Basic Category
-cars=fullOverSampledDf[fullOverSampledDf['BasicCategoryNum']==4]
+#Create Passenger Vehicle Basic Categoryz
+cars=underSampledDf[underSampledDf['BasicCategoryNum']==4]
 cars = cars.assign(BasicCategory='Passenger Vehicle')
 #Create Bicycle Basic Category
-bikes=fullOverSampledDf[fullOverSampledDf['BasicCategoryNum']==0]
+bikes=underSampledDf[underSampledDf['BasicCategoryNum']==0]
 bikes = bikes.assign(BasicCategory='Bicycle')
 #Create Motorcycle Basic Category
-motorcycles=fullOverSampledDf[fullOverSampledDf['BasicCategoryNum']==3]
+motorcycles=underSampledDf[underSampledDf['BasicCategoryNum']==3]
 motorcycles = motorcycles.assign(BasicCategory='Motorcycle')
 #Create tractor Basic Category
-tractors=fullOverSampledDf[fullOverSampledDf['BasicCategoryNum']==6]
+tractors=underSampledDf[underSampledDf['BasicCategoryNum']==6]
 tractors = tractors.assign(BasicCategory='Tractor')
 #Create Tractor Basic Category
-trailers=fullOverSampledDf[fullOverSampledDf['BasicCategoryNum']==7]
+trailers=underSampledDf[underSampledDf['BasicCategoryNum']==7]
 trailers = trailers.assign(BasicCategory='Trailer')
 #Create Bus Basic Category
-Buses=fullOverSampledDf[fullOverSampledDf['BasicCategoryNum']==1]
+Buses=underSampledDf[underSampledDf['BasicCategoryNum']==1]
 Buses = Buses.assign(BasicCategory='Bus')
 #Create Construction Vehicle Basic Category
-constructionVehicles=fullOverSampledDf[fullOverSampledDf['BasicCategoryNum']==2]
+constructionVehicles=underSampledDf[underSampledDf['BasicCategoryNum']==2]
 constructionVehicles = constructionVehicles.assign(BasicCategory='Construction Vehicle')
 
-reconstructedOverSampledDF=peds.append([cars, bikes,motorcycles,tractors,trailers,Buses,constructionVehicles])
+reconstructedUnderSampledDF=peds.append([cars, bikes,motorcycles,tractors,trailers,Buses,constructionVehicles])
 
-reconstructedOverSampledDF['BasicCategory'].value_counts()
-reconstructedOverSampledDF.to_csv('OverSampled_FullDataSet.csv')
+reconstructedUnderSampledDF['BasicCategory'].value_counts()
+reconstructedUnderSampledDF.to_csv('UnderSampled_FullDataSet.csv')
 # =============================================================================
 # 
 # SPLIT DATA INTO TEST AND TRAINING SETS AND SAVE IT
 # 
 # =============================================================================
-train,test = train_test_split(reconstructedOverSampledDF, test_size = 0.20)
-train.to_csv('OverSampled_TrainSet.csv')
-test.to_csv('OverSampled_TestSet.csv')
+train,test = train_test_split(reconstructedUnderSampledDF, test_size = 0.20)
+train.to_csv('UnderSampled_TrainSet.csv')
+test.to_csv('UnderSampled_TestSet.csv')
